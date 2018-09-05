d17d55408b422db6d3add45fa77b18e079fd7ac2
mhartry <meghan-hartry@uiowa.edu>
2018-09-05 17:26:57 -0500

    updated

--- Arduino/lab1/lab1.ino
+++ Arduino/lab1/lab1.ino
@@ -1,10 +1,9 @@
-/*#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
 #define FIREBASE_HOST "senior-design-lab-1.firebaseio.com"
 #define FIREBASE_AUTH "ayPFppZ85ao3smSbXmjKHd4mDZ4EnPjzPGI8bUH0"
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
-*/
+#define WIFI_SSID "meghan"
+#define WIFI_PASSWORD "phonepass"
 
 //libraries
 #include <OneWire.h> 
@@ -21,7 +20,10 @@ void setup() {
   sensors.begin();
 
 
-/* https://medium.com/konel/firebase-with-realtime-database-for-iot-applications-e615a7057a48
+ //https://medium.com/konel/firebase-with-realtime-database-for-iot-applications-e615a7057a48
+ //https://create.arduino.cc/projecthub/TheGadgetBoy/ds18b20-digital-temperature-sensor-and-arduino-9cc806
+ //https://www.roboshala.com/nodemcu-esp8266-googles-firebase/
+ 
    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
    Serial.print(“connecting”);
    while (WiFi.status() != WL_CONNECTED) {
@@ -29,7 +31,7 @@ void setup() {
    }
    delay(500);
    Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-*/
+   
 }
 
 void loop() {
@@ -39,4 +41,7 @@ void loop() {
    Serial.print("Temperature is: "); 
    Serial.print(sensors.getTempCByIndex(0));
    delay(1000); 
+
+   float t = sensors.getTempFByIndex(0);
+   Firebase.setFloat ("Temperature", t);
 }
