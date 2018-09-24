236ab5902da22bc7701592b5a5af86e7670b72b0
KelseyCulp <36823555+KelseyCulp@users.noreply.github.com>
2018-09-23 22:03:15 -0500

    webpage

---
+++ Lab1Webpage/public/assets/scripts/app.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCmnqnEBfqzJHz51xdrfk4mNN4U1lJhFiw",
+  authDomain: "lab1-2018.firebaseapp.com",
+  databaseURL: "https://lab1-2018.firebaseio.com",
+  projectId: "lab1-2018",
+  storageBucket: "lab1-2018.appspot.com",
+  messagingSenderId: "964922543577"
+};
+firebase.initializeApp(config);
